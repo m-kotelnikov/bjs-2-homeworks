a91a93018009f89cc9b26370e11467f2525bce91
Mihail Kotelnikov <m9165830551@gmail.com>
2026-06-28 17:39:52 +0300

    Первое задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
+    return arr;
+  }
+
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  }
+
+  arr.push(
+    (-b + Math.sqrt(discriminant)) / (2 * a),
+    (-b - Math.sqrt(discriminant)) / (2 * a),
+  );
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercent = percent / 100 / 12;
+
+  let creditBody = amount - contribution;
+
+  let monthlyPayment =
+    creditBody *
+    (monthlyPercent +
+      monthlyPercent / ((1 + monthlyPercent) ** countMonths - 1));
+
+  let totalAmount = monthlyPayment * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
