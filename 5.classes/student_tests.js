b200d931bab0956f133ffe31690b143058f090c3
Mihail Kotelnikov <m9165830551@gmail.com>
2026-07-19 19:37:11 +0300

    Пятое задание готово

--- 5.classes/student_tests.js
+++ 5.classes/student_tests.js
@@ -1,54 +1,54 @@
-describe('Домашнее задание к лекции 5 «Классы». Дополнительное задание', () => {
-  describe('Задача №3', () => {
+describe("Домашнее задание к лекции 5 «Классы». Дополнительное задание", () => {
+  describe("Задача №3", () => {
     let student;
-  
-    beforeEach(function(){
+
+    beforeEach(function () {
       student = new Student("Иван Петров");
     });
 
-    it('создание объекта Student', () => {
+    it("создание объекта Student", () => {
       expect(student).toBeDefined();
       expect(student.name).toEqual("Иван Петров");
       expect(student.marks).toEqual({});
     });
 
-    it('добавление оценок по разным предметам', () => {
+    it("добавление оценок по разным предметам", () => {
       student.addMark(3, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
+      expect(student.marks).toEqual({ математика: [3] });
       student.addMark(5, "математика");
-      expect(student.marks).toEqual({"математика": [3, 5]});
+      expect(student.marks).toEqual({ математика: [3, 5] });
       student.addMark(5, "физика");
-      expect(student.marks).toEqual({"математика": [3, 5], "физика": [5]});
+      expect(student.marks).toEqual({ математика: [3, 5], физика: [5] });
     });
 
-    it('невозможность добавления некорректных оценок', () => {
+    it("невозможность добавления некорректных оценок", () => {
       student.addMark(0, "математика");
       expect(student.marks).toEqual({});
       student.addMark(3, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
+      expect(student.marks).toEqual({ математика: [3] });
       student.addMark(10, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
+      expect(student.marks).toEqual({ математика: [3] });
       student.addMark(7, "физика");
-      expect(student.marks).toEqual({"математика": [3]});
+      expect(student.marks).toEqual({ математика: [3] });
     });
 
-    it('подсчёт средней оценки по предмету', () => {
+    it("подсчёт средней оценки по предмету", () => {
       student.addMark(3, "математика");
       student.addMark(5, "математика");
       expect(student.getAverageBySubject("математика")).toEqual(4);
     });
 
-    it('подсчёт средней оценки по несуществующему предмету', () => {
+    it("подсчёт средней оценки по несуществующему предмету", () => {
       student.addMark(3, "математика");
       student.addMark(5, "математика");
       expect(student.getAverageBySubject("программирование")).toEqual(0);
     });
 
-    it('подсчёт общей средней оценки пустого объекта оценок', () => {
+    it("подсчёт общей средней оценки пустого объекта оценок", () => {
       expect(student.getAverage()).toEqual(0);
     });
 
-    it('подсчёт общей средней оценки', () => {
+    it("подсчёт общей средней оценки", () => {
       student.addMark(3, "математика");
       student.addMark(5, "математика");
       student.addMark(5, "история");
@@ -57,3 +57,43 @@ describe('Домашнее задание к лекции 5 «Классы». Д
     });
   });
 });
+
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {};
+  }
+
+  addMark(mark, subject) {
+    if (mark < 2 || mark > 5) {
+      return;
+    }
+    if (!this.marks[subject]) {
+      this.marks[subject] = [];
+    }
+    this.marks[subject].push(mark);
+  }
+
+  getAverageBySubject(subject) {
+    if (!this.marks[subject]) {
+      return 0;
+    }
+    const subjectMarks = this.marks[subject];
+    const average =
+      subjectMarks.reduce((sum, mark) => sum + mark, 0) / subjectMarks.length;
+    return average;
+  }
+
+  getAverage() {
+    if (Object.keys(this.marks).length === 0) {
+      return 0;
+    }
+    let totalSum = 0;
+    let totalMarks = 0;
+    for (const [subject, marks] of Object.entries(this.marks)) {
+      totalSum += marks.reduce((sum, mark) => sum + mark, 0);
+      totalMarks += marks.length;
+    }
+    return totalSum / totalMarks;
+  }
+}
